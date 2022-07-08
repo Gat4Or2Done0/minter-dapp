9f256568676dcb1d933df6ef0d0416164dccad96
Garrett Everling <nutheads@yahoo.com>
2022-07-08 18:50:47 -0400

    UPDATE

--- backend/src/config.js
+++ backend/src/config.js
@@ -66,7 +66,7 @@ const TOKENS_PER_MINT = "10"; // maximum number of NFTs a user can mint in a sin
 const PUBLIC_MINT_START_DATE = "2022-08-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-20T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = "1000"; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xF4273496e59C65111701C2677de8A3137f2d2491"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
